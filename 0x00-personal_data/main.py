35162857a027c28c107993c2c730d910e4d1b6ff
Listus97 <namurendecallistus@gmail.com>
2024-02-07 21:35:54 +0000

    Encrypting passwords, Check valid password

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -39,4 +39,26 @@ PII_FIELDS = __import__('filtered_logger').PII_FIELDS
 print(get_logger.__annotations__.get('return'))
 print("PII_FIELDS: {}".format(len(PII_FIELDS)))
 
+#!/usr/bin/env python3
+"""
+Main file
+"""
 
+hash_password = __import__('encrypt_password').hash_password
+
+password = "MyAmazingPassw0rd"
+print(hash_password(password))
+print(hash_password(password))
+
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
+
+password = "MyAmazingPassw0rd"
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
